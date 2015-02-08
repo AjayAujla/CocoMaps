39df48481eb03a272c8993ba4214db4d1b11323d
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2015-02-08 04:22:36 -0500

    Reindentation upon saving, change application title to CocoMaps, open map directly to SGW campus, create button for Concordia Services

--- CocoMaps.Shared/Pages/Root.cs
+++ CocoMaps.Shared/Pages/Root.cs
@@ -10,22 +10,22 @@ namespace CocoMaps.Shared.Pages
 {
 	public class RootPage : MasterDetailPage
 	{
-		MenuOptions previousItem;
+		IMenuOptions previousItem;
 
 		public RootPage ()
 		{
 
 			var optionsPage = new MenuPage { Icon = "settings.png", Title = "menu" };
 
-			optionsPage.Menu.ItemSelected += (sender, e) => NavigateTo(e.SelectedItem as MenuOptions);
+			optionsPage.Menu.ItemSelected += (sender, e) => NavigateTo (e.SelectedItem as IMenuOptions);
 
 			Master = optionsPage;
 
-			NavigateTo(optionsPage.Menu.ItemsSource.Cast<MenuOptions>().First());
+			NavigateTo (optionsPage.Menu.ItemsSource.Cast<IMenuOptions> ().First ());
 
 		}
 
-		void NavigateTo(MenuOptions option)
+		void NavigateTo (IMenuOptions option)
 		{
 			if (previousItem != null)
 				previousItem.Selected = false;
@@ -33,53 +33,47 @@ namespace CocoMaps.Shared.Pages
 			option.Selected = true;
 			previousItem = option;
 
-			var displayPage = PageForOption(option);
+			var displayPage = PageForOption (option);
 
 			#if WINDOWS_PHONE
 			Detail = new ContentPage();//work around to clear current page.
 			#endif
-			Detail = new NavigationPage(displayPage)
-			{
-				BarBackgroundColor = Helpers.Color.Maroon.ToFormsColor(),
+			Detail = new NavigationPage (displayPage) {
+				BarBackgroundColor = Helpers.Color.Maroon.ToFormsColor (),
 			};
 
 
 			IsPresented = false;
 		}
 
-		Page PageForOption (MenuOptions option)
+		Page PageForOption (IMenuOptions option)
 		{
 			// TODO: Refactor this to the Builder pattern (see ICellFactory).
-			if (option.Title == "Current Location")
-			{
-				return new MasterPage(option);
+			if (option.Title == "Current Location") {
+				return new MasterPage (option);
 			}
-			if (option.Title == "Points of Interest")
-			{
-				return new MasterPage(option);
+			if (option.Title == "Campus Maps") {
+				return new MasterPage (option);
 			}
-			if (option.Title == "Campus Maps")
-			{
-				return new MasterPage(option);
+			if (option.Title == "Points of Interest") {
+				return new MasterPage (option);
 			}
-			if (option.Title == "Building Directions")
-			{
-				return new MasterPage(option);
+			if (option.Title == "Concordia Services") {
+				return new MasterPage (option);
 			}
-			if (option.Title == "Indoor Directions")
-			{
-				return new MasterPage(option);
+			if (option.Title == "Building Directions") {
+				return new MasterPage (option);
 			}
-			if (option.Title == "Calendar")
-			{
-				return new MasterPage(option);
+			if (option.Title == "Indoor Directions") {
+				return new MasterPage (option);
 			}
-			if (option.Title == "Settings")
-			{
-				return new MasterPage(option);
+			if (option.Title == "Calendar") {
+				return new MasterPage (option);
 			}
-			throw new NotImplementedException("Unknown menu option: " + option.Title);
+			if (option.Title == "Settings") {
+				return new MasterPage (option);
+			}
+			throw new NotImplementedException ("Unknown menu option: " + option.Title);
 		}
 	}
 }
-
--- CocoMaps.Shared/ViewsModel/MenuItems.cs
+++ CocoMaps.Shared/ViewsModel/MenuItems.cs
@@ -8,55 +8,73 @@ using System.Linq;
 namespace CocoMaps.Models
 {
 
-	public class Location_MenuOption : MenuOptions
+	public class Location_MenuOption : IMenuOptions
 	{
 		public override string Title { get { return "Current Location"; } }
+
 		public override string Icon { get { return "locationIcon.png"; } }
 	}
 
-	public class Campus_MenuOption : MenuOptions
+	public class Campus_MenuOption : IMenuOptions
 	{
 		public override string Title { get { return "Campus Maps"; } }
+
 		public override string Icon { get { return "campusIcon.png"; } }
 	}
 
-	public class pInterest_MenuOption : MenuOptions
+	public class pInterest_MenuOption : IMenuOptions
 	{
 		public override string Title { get { return "Points of Interest"; } }
+
 		public override string Icon { get { return "poiIcon.png"; } }
 	}
 
-	public class bDirections_MenuOption : MenuOptions
+	public class ConcordiaServices_MenuOption : IMenuOptions
+	{
+		public override string Title { get { return "Concordia Services"; } }
+
+		public override string Icon { get { return "poiIcon.png"; } }
+	}
+
+	public class bDirections_MenuOption : IMenuOptions
 	{
 		public override string Title { get { return "Building Directions"; } }
+
 		public override string Icon { get { return "buildingIcon.png"; } }
 	}
 
-	public class iDirections_MenuOption : MenuOptions
+	public class iDirections_MenuOption : IMenuOptions
 	{
 		public override string Title { get { return "Indoor Directions"; } }
+
 		public override string Icon { get { return "classIcon.png"; } }
 	}
 
-	public class Calendar_MenuOption : MenuOptions
+	public class Calendar_MenuOption : IMenuOptions
 	{
 		public override string Title { get { return "Calendar"; } }
+
 		public override string Icon { get { return "calendarIcon.png"; } }
 	}
 
-	public class Settings_MenuOption : MenuOptions
+	public class Settings_MenuOption : IMenuOptions
 	{
 		public override string Title { get { return "Settings"; } }
+
 		public override string Icon { get { return "settingsIcon.png"; } }
 	}
 
-	public  abstract class MenuOptions
+	public abstract class IMenuOptions
 	{
 		public virtual string Title { get { return Title; } }
+
 		public virtual int Count { get; set; }
+
 		public virtual bool Selected { get; set; }
+
 		public virtual string Icon { get { return Icon; } }
-		public ImageSource IconSource { get { return ImageSource.FromFile(Icon); } }
+
+		public ImageSource IconSource { get { return ImageSource.FromFile (Icon); } }
 	}
 		
 }
