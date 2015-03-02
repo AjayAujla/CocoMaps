1c487abbc0b9328d3351a6d2ef1e443f7ff24a03
Abhi728 <abhi564@yahoo.ca>
2015-03-02 13:29:05 -0500

    Next Class Implementation v1
    Adding the Next Class Page with test Values.
    
    Map directions to be implemented in v2.

--- CocoMaps.Shared/Views/Pages/Menu.cs
+++ CocoMaps.Shared/Views/Pages/Menu.cs
@@ -20,6 +20,7 @@ namespace CocoMaps.Shared.Pages
 			OptionItems.Add (new Location_MenuOption ());
 			OptionItems.Add (new Campus_MenuOption ());
 			OptionItems.Add (new pInterest_MenuOption ());
+			OptionItems.Add (new NextClass_MenuOption ());
 			OptionItems.Add (new ConcordiaServices_MenuOption ());
 			OptionItems.Add (new bDirections_MenuOption ());
 			OptionItems.Add (new iDirections_MenuOption ());
---
+++ CocoMaps.Shared/Views/Pages/NextClass/NextClass.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Xamarin.Forms;
+using CocoMaps.Shared.Pages;
+using CocoMaps.Shared.ViewModels;
+using CocoMaps.Models;
+using Xamarin.Forms.Maps;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace CocoMaps.Shared
+{
+	public class NextClass : ContentPage
+	{
+		public NextClass (IMenuOptions menuItem)
+		{
+
+			string startLocation;
+			string destination;
+
+			// Test
+
+			string testClass = "SGW-FG-965";
+			startLocation = "7141 Sherbrooke Street W.";
+
+			destination = getClassLocation (testClass);
+
+			startLocation = startLocation + " Montreal QC";
+			destination = destination + " Montreal QC";
+
+
+			Label label0 = new Label
+			{
+				FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+				FontAttributes = FontAttributes.Bold,
+				Text = "Next Class : "
+			};
+
+
+			Label label1 = new Label
+			{
+				Text = testClass ,
+
+				Font = Font.SystemFontOfSize(NamedSize.Large) 
+			};
+
+			Label label2 = new Label
+			{
+				FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+				FontAttributes = FontAttributes.Bold,
+				Text = "Destination : "
+			};
+
+			Label label3 = new Label
+			{
+				Text = destination ,
+
+				Font = Font.SystemFontOfSize(NamedSize.Large)
+			};
+
+			Label label4 = new Label
+			{
+				FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+				FontAttributes = FontAttributes.Bold,
+				Text = "Current Location : "
+			};
+
+			Label label5 = new Label
+			{
+				Text = startLocation ,
+
+				Font = Font.SystemFontOfSize(NamedSize.Large)
+			};
+
+			this.Content = new StackLayout
+			{
+				Children =
+				{
+					label0,
+					label1,
+					label2,
+					label3,
+					label4,
+					label5
+				}
+				};
+
+
+
+		}
+
+
+		public string getClassLocation(string dest)
+		{
+			string destination = dest.Trim().ToUpper();
+
+			string[] destination_array = destination.Split('-');
+
+			string campusLoc = destination_array [0];
+
+			string roomLoc = destination_array [1];
+
+
+			BuildingRepository BR = new BuildingRepository();
+
+			Campus campus = BR.GetCampusByCode(campusLoc);
+
+			Building building = campus.GetBuildingByCode(roomLoc);
+
+
+			return building.Address;
+
+		}
+
+
+
+	}
+}
+
