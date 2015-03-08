2d9ab540819e4ebd4e71a2c2f726d9ca404571d0
mdubey2014 <dubeymridula@gmail.com>
2015-03-08 18:17:42 -0400

    COCO-45 Removed the changes done in code to complete menu item page. It will be worked on later

--- CocoMaps.Shared/Views/Pages/Menu.cs
+++ CocoMaps.Shared/Views/Pages/Menu.cs
@@ -17,13 +17,13 @@ namespace CocoMaps.Shared.Pages
 
 		public MenuPage ()
 		{
-			//OptionItems.Add (new Location_MenuOption ());
+			OptionItems.Add (new Location_MenuOption ());
 			OptionItems.Add (new Campus_MenuOption ());
 			OptionItems.Add (new pInterest_MenuOption ());
 			OptionItems.Add (new NextClass_MenuOption ());
 			OptionItems.Add (new ConcordiaServices_MenuOption ());
-			//OptionItems.Add (new bDirections_MenuOption ());
-			//OptionItems.Add (new iDirections_MenuOption ());
+			OptionItems.Add (new bDirections_MenuOption ());
+			OptionItems.Add (new iDirections_MenuOption ());
 			OptionItems.Add (new Calendar_MenuOption ());
 			OptionItems.Add (new Settings_MenuOption ());
 
