7429547d82bdef57bd90e918d1e87f0b8809e8d8
dfdp <blowout180@hotmail.com>
2015-03-14 23:24:32 -0400

    COCO-18 working on it
    Working on getting notifications to work.
    Have two different ways of getting it to work at the moment (taken from
    sources).
    Trying to figure out the best for our application.

---
+++ CocoMaps.Android/AndroidNotifications/AndroidReminderService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.Support.V4.App;
+using Android.Content;
+using Android.App;
+using Xamarin.Forms;
+using Android.Graphics;
+using Android.OS;
+
+namespace CocoMaps.Android
+{
+	public class AndroidReminderService 
+	{
+
+		public void Remind (DateTime dateTime, string title, string message)
+		{
+
+			Intent alarmIntent = new Intent(Forms.Context, typeof(AlarmReceiver));
+			alarmIntent.PutExtra ("message", message);
+			alarmIntent.PutExtra ("title", title);
+
+			PendingIntent pendingIntent = PendingIntent.GetBroadcast(Forms.Context, 0, alarmIntent, PendingIntentFlags.UpdateCurrent);
+			AlarmManager alarmManager = (AlarmManager) Forms.Context.GetSystemService(Context.AlarmService);
+
+			//TODO: For demo set after 5 seconds.
+			alarmManager.Set(AlarmType.ElapsedRealtime, SystemClock.ElapsedRealtime () + 5 * 1000, pendingIntent);
+	
+
+		}
+			
+	}
+}
+
