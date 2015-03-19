3b3695a78e160f654bd1b329368257cfd3d9321c
ConfusedGiant <dorias.ahmed@gmail.com>
2015-03-19 17:45:53 -0400

    COCO 18 - Disable/enable button should work
    Working on the enable/disable button.
    
    Authors: Ahmed, David D.

--- CocoMaps.Android/AndroidNotifications/AlarmReceiver.cs
+++ CocoMaps.Android/AndroidNotifications/AlarmReceiver.cs
@@ -9,30 +9,33 @@ namespace CocoMaps.Android
 	[BroadcastReceiver]
 	public class AlarmReceiver : BroadcastReceiver 
 	{
+		public static bool notificationFlag = true;
+
 		public override void OnReceive (Context context, Intent intent)
 		{
+			if (notificationFlag == true) {
+				var message = intent.GetStringExtra ("message");
+				var title = intent.GetStringExtra ("title");
 
-			var message = intent.GetStringExtra ("message");
-			var title = intent.GetStringExtra ("title");
-
-			var notIntent = new Intent (context, typeof(MainActivity));
-			var contentIntent = PendingIntent.GetActivity (context, 0, notIntent, PendingIntentFlags.CancelCurrent);
-			var manager = NotificationManagerCompat.From (context);
+				var notIntent = new Intent (context, typeof(MainActivity));
+				var contentIntent = PendingIntent.GetActivity (context, 0, notIntent, PendingIntentFlags.CancelCurrent);
+				var manager = NotificationManagerCompat.From (context);
 			
-			//Generate a notification with just short text and small icon
-			var builder = new NotificationCompat.Builder (context)
+				//Generate a notification with just short text and small icon
+				var builder = new NotificationCompat.Builder (context)
 							.SetContentIntent (contentIntent)
 							.SetSmallIcon (Resource.Drawable.splash)
-							.SetContentTitle(title)
-							.SetContentText(message)
-							.SetWhen(Java.Lang.JavaSystem.CurrentTimeMillis())
-							.SetAutoCancel(true)
-				.SetStyle(new NotificationCompat.BigTextStyle().BigText(message));
+							.SetContentTitle (title)
+							.SetContentText (message)
+							.SetWhen (Java.Lang.JavaSystem.CurrentTimeMillis ())
+							.SetAutoCancel (true)
+				.SetStyle (new NotificationCompat.BigTextStyle ().BigText (message));
 	
 			
 			
-						var notification = builder.Build();
-						manager.Notify(0, notification);
+				var notification = builder.Build ();
+				manager.Notify (0, notification);
+			}
 		}
 	}
 }
