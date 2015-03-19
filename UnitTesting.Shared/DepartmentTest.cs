59b1df79809b23033a7a82b1b54d00a8192fca06
DavidPaparo <david_paparo@hotmail.com>
2015-03-19 01:21:08 -0400

    Unit Test Re-implemented
    Due to massive conflict number (old version), unit tests have been
    re-implemented here. Will Inject as to avoid the issue repeating itself.

---
+++ UnitTesting.Shared/DepartmentTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using CocoMaps.Shared;
+
+namespace UnitTesting.Shared
+{
+	[TestFixture ()]
+	public class BuildingTest
+	{
+		[Test ()]
+		public void TestSetandGetMethods ()
+		{
+			Department d = new Department();
+
+			d.Name = "Department of Computer Science & Software Engineering";
+			Assert.Equals ("Department of Computer Science & Software Engineering", d.Name);
+
+			d.URI = "http://www.concordia.ca/encs/computer-science-software-engineering.html";
+			Assert.Equals ("http://www.concordia.ca/encs/computer-science-software-engineering.html", d.URI);
+		}
+	}
+}
