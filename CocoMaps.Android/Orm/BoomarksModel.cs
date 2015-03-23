f4255122736cce2563372cfef7eb1893bab88d5e
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2015-03-23 03:54:07 -0400

    [COCO-23] Change directory of Bookmarks Model

---
+++ CocoMaps.Android/Orm/BoomarksModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+using CocoMaps.Shared.Pages;
+using CocoMaps.Shared.ViewModels;
+using Xamarin.Forms.Maps;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+
+namespace CocoMaps.Shared
+{
+	public class BookmarkItems
+	{
+		public BookmarkItems ()
+		{
+
+		}
+
+		public BookmarkItems (string name, string address, double lat, double lon, string bicon)
+		{
+			this.bName = name;
+			this.bAddress = address;
+			this.bLat = lat;
+			this.bLon = lon;
+			this.IconSource = ImageSource.FromFile (bicon);
+		}
+
+		public int ID {
+			get;
+			set;
+		}
+
+		public string bName { private set; get; }
+
+		public string bAddress { private set; get; }
+
+		public double bLat { private set; get; }
+
+		public double bLon { private set; get; }
+
+		public ImageSource IconSource { private set; get; }
+
+		//public Color BoxColor { private set; get; }
+
+
+	}
+}
